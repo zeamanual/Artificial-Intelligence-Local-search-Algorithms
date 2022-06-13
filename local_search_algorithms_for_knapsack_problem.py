4167cba443f8bc67c0f00bc9c888b6e6169f2ca6
abe <zeamanualfeleke@gmail.com>
2022-06-13 12:09:02 +0300

    genetic  search algorithm along with command line input handler functions implemented for knapsack problem

--- local_search_algorithms_for_knapsack_problem.py
+++ local_search_algorithms_for_knapsack_problem.py
@@ -225,4 +225,197 @@ class SimulatedAnnealing:
             iteration_count+=1
         return self.best_solution,self.best_soluion_value
      
+            
+            
+class GeneticAlgorithm:
+    def __init__(self,items,weight,generation_count,population_size):
+        self.generation_count= generation_count
+        self.population_size=population_size
+        self.items_detail = items.copy()
+        self.list_of_items= list(self.items_detail.keys())
+        self.weight = weight
+        
+
+    def create_initial_population(self,size):
+        initial_population = []
+        for count in range(size):
+            solution =[]
+            has_free_space=True
+            while has_free_space:
+                random_index = random.randint(0,len(self.list_of_items)-1)
+                solution.append(self.list_of_items[random_index])
+                temp_duplicate_count = self.get_max_item_occurance(solution)
+                while temp_duplicate_count >3:
+                    solution.pop()
+                    random_index = random.randint(0,len(self.list_of_items)-1)
+                    solution.append(self.list_of_items[random_index])
+                    temp_duplicate_count = self.get_max_item_occurance(solution)           
+                temp_weight=self.get_solution_weight(solution)
+                if(temp_weight>self.weight):
+                    solution.pop()
+                    has_free_space=False
+            initial_population.append(solution)
+        return initial_population
+       
+    def get_max_item_occurance(self,solution):
+        max_duplicate_count=0
+        for i in range(len(solution)):
+            temp_duplicate_count=0
+            for j in range(len(solution)):
+                if(solution[i]==solution[j]):
+                    temp_duplicate_count+=1
+            if(temp_duplicate_count > max_duplicate_count):
+                max_duplicate_count=temp_duplicate_count
+        return max_duplicate_count
+          
+
+    def get_fitness_score(self,solution):
+        value = 0
+        weight=0
+        if(self.get_max_item_occurance(solution) > 3):
+            return 0
+        for item in solution:
+            value+=self.items_detail[item][1]
+            weight+=self.items_detail[item][0]
+        if(weight>self.weight):
+            return 0
+        else:
+            return value
+
+    def get_solution_weight(self,solution):
+        weight=0
+        for item in solution:
+            weight+=self.items_detail[item][0]
+        return weight
  
+    def cross_over(self,parents):
+        parent_one=parents[0]
+        parent_two=parents[1]
+        solution_length = len(parent_one)
+        offspring1=parent_one[:solution_length//2]
+        offspring2 = parent_two[:solution_length//2]
+        
+        for gene in parent_two:
+            if gene not in offspring1:
+                offspring1.append(gene)
+        for gene in parent_one:
+            if gene not in offspring2:
+                offspring2.append(gene)
+        return [offspring1,offspring2]
+
+    def mutate_solution(self,solution):
+        solution_length=len(solution)
+        rand_index_one = random.randint(0,solution_length-1)
+        rand_index_two = random.randint(0,len(self.list_of_items)-1)
+        solution[rand_index_one]= self.list_of_items[rand_index_two]
+        
+        return solution     
+        
+    def start(self):
+        temprature = 60
+        final_temprature=10
+        self.best_solution=None
+        self.best_solution_fitness_score = 0
+        
+        population = self.create_initial_population(self.population_size)
+        for count in range(self.generation_count):
+            tested_population_detail = []
+            for solution in population:
+                score = self.get_fitness_score(solution)
+                tested_population_detail.append([score,solution])
+            tested_population_detail.sort()
+            
+            top_solutions_detail = tested_population_detail[-100:]
+            temp_best_solution=top_solutions_detail[-1]
+            
+            if(temp_best_solution[0]>self.best_solution_fitness_score):
+                self.best_solution=temp_best_solution[1]
+                self.best_solution_fitness_score=temp_best_solution[0]
+                temprature=60
+            else:
+                temprature-=10
+                if(temprature<10):
+                    break
+    
+            best_solutions = []
+            for solution in top_solutions_detail:
+                best_solutions.append(solution[1])
+            
+            population=[]
+            for size in range(self.population_size//2):
+                parent_one = best_solutions[ random.randint(0,len(best_solutions)-1)]
+                parent_two = best_solutions[ random.randint(0,len(best_solutions)-1)]
+                
+                offsprings = self.cross_over([parent_one,parent_two])
+                offsprings[0]=self.mutate_solution(offsprings[0])
+                offsprings[1]=self.mutate_solution(offsprings[1])
+                population.append(offsprings[0])
+                population.append(offsprings[1])
+
+        return self.best_solution,self.best_solution_fitness_score
+                
+        
+
+ #load all data from file
+item_quantity=0
+def load_data(filename):
+    
+    #Reading contents of items file
+    items_file= open(filename)
+    global required_weight , items
+    
+    items={}
+    read_line=items_file.readline()
+    required_weight = int(read_line)
+    
+    read_line = items_file.readline()
+    read_line = items_file.readline()
+    while(read_line):
+        read_line=read_line.split(',')
+        items[read_line[0]]=[float(read_line[1]),float(read_line[2])]
+        read_line=items_file.readline()
+    items_file.close()
+
+
+def get_huristic_value(location_one,location_two):
+    RADIUS=6373.0
+    latitude_one = math.radians(location_one[0])
+    longitude_one = math.radians(location_one[1])
+    latitude_two = math.radians(location_two[0])
+    longitude_two = math.radians(location_two[1])
+
+    diffrence_in_longitude = longitude_two-longitude_one
+    diffrence_in_latitude = latitude_two -latitude_one
+
+    a = math.pow((math.sin(diffrence_in_latitude/2)),2) + math.cos(latitude_one)*math.cos(latitude_two)*math.pow((math.sin(diffrence_in_longitude/2)),2)
+    c = 2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+
+    distance_between = RADIUS * c
+    return distance_between
+  
+    
+def main():
+    parser = argparse.ArgumentParser(description="Get file and algorithm name")
+    parser.add_argument('--algorithm',help='Enter the name of the algorithm to use such as ga, hl, sa')
+    parser.add_argument('--file',help='Enter the name of the file to read from')
+    arguments = parser.parse_args()
+
+    filename = arguments.file
+    algorithm = arguments.algorithm
+    load_data(filename)
+
+    if(algorithm=='ga'):
+        genetic_algorithm = GeneticAlgorithm(items,required_weight,20,200)
+        result = genetic_algorithm.start()
+        print(result)
+       
+    elif(algorithm=='hl'):
+        hill_climbing_algorithm = HillClimbing(items,required_weight)
+        result = hill_climbing_algorithm.start()
+        print(result)
+    elif(algorithm=='sa'):
+        simulated_annealing = SimulatedAnnealing(items,required_weight)
+        result = simulated_annealing.start()
+        print(result)
+    
+main()
